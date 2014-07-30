7bd1bf467ff8f62e0687f71d3e7c368f62a7df31
Paolo Umali <dev@paoloumali.com>
2014-07-31 04:04:00 +0800

    start paoloumali/user-management

---
+++ src/Paoloumali/UserManagement/UserManagementServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Paoloumali\UserManagement;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserManagementServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('paoloumali/user-management');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
